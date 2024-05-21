254a1d8a25d90120534ff39033abc65d1963d1ed
moreirarj <gmoreirarjjpa@gmail.com>
2024-05-21 15:54:45 -0300

    Added selectors list for login spec[2]

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -13,7 +13,10 @@ describe('Orange HRM Tests',() => {
      lastNameField: "[name='lastName']",
      genericField: ".oxd-input--active",
      dateField: "[placeholder='yyyy-dd-mm']",
-     dateCloseButton: ".--close",
+     genericComboBox: ".oxd-select-text--active",
+     secondIntemCombobox:'.oxd-select-dropdown > :nth-child(11)',
+     thirdIntemCombobox:'.oxd-select-dropdown > :nth-child(3)',
+     //dateCloseButton: ".--close",
      submitButton: "[type='submit']",
      }
 it.only ('User Info Update - Success',() =>{
@@ -26,13 +29,20 @@ it.only ('User Info Update - Success',() =>{
     cy.get(selectorsList.myInfoButton).click()
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('EmplIdTest')
-    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(5).clear().type('DriverLicenseNumberTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('2024-08-12')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    
+    cy.get(selectorsList.genericField).eq(4).clear().type('EmplIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('DriverLicenseNumberTest')
+    cy.get(selectorsList.genericField).eq(7).clear().type('2024-08-12')
+   // cy.get(selectorsList.genericField).eq(8).clear().type('ssnNumberTest')
+    //cy.get(selectorsList.genericField).eq(9).clear().type('sinNumberTest')
+    //cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click({force: true})
+
+
+    cy.get(selectorsList.genericComboBox).eq(0).click({force: true})
+    cy.get(selectorsList.secondIntemCombobox).click()
+    cy.get(selectorsList.genericComboBox).eq(1).click({force: true})
+    cy.get(selectorsList.thirdIntemCombobox).click()
     
     
     
