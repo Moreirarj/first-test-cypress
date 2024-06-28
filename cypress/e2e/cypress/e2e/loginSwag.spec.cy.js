77c30e7789d22af2df261f5cb55ee2f56db98943
Gabriel Moreira Flores <gflores@band.com.br>
2024-06-28 19:26:44 -0300

    Exercicio

--- cypress/e2e/cypress/e2e/loginSwag.spec.cy.js
+++ cypress/e2e/cypress/e2e/loginSwag.spec.cy.js
@@ -1,25 +1,35 @@
-describe ('Swag Labs', () => {
+describe ('SWAGLABS', () => {
     Cypress.config('defaultCommandTimeout', 4000)  // 4 segundos
-    
+
     it('Login With Success',() =>{
     cy.visit ('https://www.saucedemo.com/v1/index.html')
     cy.get('[data-test="username"]').should('be.visible').type('standard_user')
     cy.get('[data-test="password"]').should('be.visible').type('secret_sauce')
     cy.get('#login-button').should('be.visible').click()
-    alert("Erro de Login");
+    
     })
+    it('New Login',() => {
+     cy.visit ('https://www.saucedemo.com/v1/index.html')
+     cy.get('[data-test="username"]').should('be.visible').type('performance_glitch_user')
+     cy.get('[data-test="password"]').should('be.visible').type('secret_sauce')
+     cy.get('#login-button').should('be.visible').click()
      
+     cy.wait(2000);
 })
-
-
-    it('Login Fail',() => {
+it('Login Fail',() => {
     cy.visit ('https://www.saucedemo.com/v1/index.html')
     cy.get('[data-test="username"]').should('be.visible').type('problem_user')
     cy.get('[data-test="password"]').should('be.visible').type('teste')
-    cy.get('#login-button').should('be.visible').click()
+    cy.get('#login-button').should('be.visible').click() 
     
-    cy.wait(2000);
+        alert("ERRO LOGIN");
     
+    
+    //cy.wait(2000);
+    
+    
+
+    })
 
 
 })
